16b55232a15075f638ff9da58034a2b79cfb69be
g16954yh <g16954yh@s20609.tsuda.ac.jp>
2018-05-22 13:18:55 +0900

    ドライバー: g16944ts ナビゲーター: g16954yh テストケース作成(インスタンス生成できず)

---
+++ Documents/workspace/PairPro5/test/junit/IntToEngTest.java
@@ -0,0 +1,14 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntToEngTest {
+
+	@Test
+	public void test() {
+		//IntToEng ite = new IntToEng();
+	}
+
+}
