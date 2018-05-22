79417961a6867abf3f265ce792a5092ed3ede8f5
g16954yh <g16954yh@s20609.tsuda.ac.jp>
2018-05-22 12:42:01 +0900

    ドライバー: g16954yh ナビゲーター: g16944ts 0~9999までの範囲で変換可能

--- Documents/workspace/PairPro5/src/IntToEng.java
+++ Documents/workspace/PairPro5/src/IntToEng.java
@@ -17,13 +17,16 @@ public class IntToEng {
     	String[] num2 = {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
     	String[] num3 = {"","hundred", "thousand"};
     	int tp,op,hp,thp;
+    	//1~19
     	if(n/20==0) return num1[n];
+    	//20~99
     	else if(n/100==0){
     		tp = n/10;
     		op = n%10;
     		if(op==0) return num2[tp];
     		return num2[tp]+"-"+num1[op];
     	}
+    	//100~999
     	else if(n/1000==0){
             hp = n/100;
     		n = n - hp * 100;
@@ -32,16 +35,17 @@ public class IntToEng {
     		if(op==0) return num1[hp]+" "+num3[1]+" "+num2[tp];
     		return num1[hp]+" "+num3[1]+" "+num2[tp]+" "+num1[op];
     	}
-    	/*else{
+
+    	//1000~9999
+    	else {
     		thp = n/1000;
     		n = n - thp * 1000;
     		hp = n/100;
     		n = n - hp * 100;
     		tp = n/10;
     		op = n%10;
-    		if(op==0) return num1[thp]+" "+num3[2]+" "+num1[hp]+" "+num3[1]+" "+num2[tp];
-    		return num1[thp]+" "+num3[2]+" "+num1[hp]+" "+num3[1]+" "+num2[tp]+" "+num1[op];
-    	} */
-    	return "";
+    		if(op==0) return num1[thp]+" "+num3[2]+" "+num1[hp]+" "+num3[1]+" and "+num2[tp];
+    		return num1[thp]+" "+num3[2]+" "+num1[hp]+" "+num3[1]+" and "+num2[tp]+" "+num1[op];
+    	} 
     }
 }
